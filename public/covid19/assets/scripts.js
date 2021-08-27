fa483e2b45b50fab4bc4d311cfa9e2b95ef54d8b
Jorge Sandoval <jorge.sandoval98@gmail.com>
2021-08-27 17:04:52 -0400

    tabla

--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -4,7 +4,7 @@ const fetchData = async (url) => {
   return await res.json();
 }
 
-const refreshChartData = (chart, data, pageSize, pageNumber) => {
+const refreshBarChartData = (chart, data, pageSize, pageNumber) => {
   const labels = data.slice((pageNumber - 1) * pageSize, pageNumber * pageSize).map((item) => item.location);
   const confirmed = data.slice((pageNumber - 1) * pageSize, pageNumber * pageSize).map((item) => item.confirmed);
   const deaths  = data.slice((pageNumber - 1) * pageSize, pageNumber * pageSize).map((item) => item.deaths);
@@ -32,12 +32,12 @@ const refreshChartData = (chart, data, pageSize, pageNumber) => {
   return labels;
 }
 
-const loadChart = (data) => {
+const generateBarChart = (data) => {
   const labels = data.slice(0, 15).map((item) => item.location);
   const confirmed = data.slice(0, 15).map((item) => item.confirmed);
   const deaths  = data.slice(0, 15).map((item) => item.deaths);
 
-  var ctx = document.getElementById('myChart');
+  const ctx = document.getElementById('myChart');
   const chart = new Chart(ctx, {
     type: 'bar',
     data: {
@@ -71,15 +71,77 @@ const loadChart = (data) => {
   return chart;
 }
 
+const generatePieChart = (confirmed = 0, deaths = 0) => {
+  const ctx = document.getElementById('myChart2');
+  const myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ['Confirmados', 'Muertos'],
+      datasets: [
+        {
+          label: 'Dataset 1',
+          data: [confirmed, deaths],
+          backgroundColor: ['red', 'gray'],
+        },
+      ]
+    },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'top',
+        },
+        title: {
+          display: true,
+          text: 'Covid-19'
+        }
+      }
+    },
+  });
+
+  return myChart;
+}
+
+const generateTable = (data) => {
+  const tableEl = document.querySelector('#tableBody');
+  data.forEach((item) => {
+    const trEl = document.createElement('tr');
+    const locationEl = document.createElement('td');
+    const confirmedEl = document.createElement('td');
+    const deathEl = document.createElement('td');
+    const detailsEl = document.createElement('td');
+    const detailsLinkEl = document.createElement('a');
+
+    locationEl.textContent = item.location;
+    confirmedEl.textContent = item.confirmed;
+    deathEl.textContent = item.deaths;
+    detailsLinkEl.href = '#'+item.location;
+    detailsLinkEl.id = item.location;
+    detailsLinkEl.classList.add('link')
+    detailsLinkEl.textContent = 'Ver Detalles';
+
+    detailsEl.appendChild(detailsLinkEl);
+
+    trEl.appendChild(locationEl);
+    trEl.appendChild(confirmedEl);
+    trEl.appendChild(deathEl);
+    trEl.appendChild(detailsEl);
+    tableEl.appendChild(trEl);
+  });
+
+}
+
 const App = async () => {
   const { data } = await fetchData('http://localhost:3000/api/total');
-  const filtered = data.filter((item) => item.confirmed > 10000);
+  const filteredData = data.filter((item) => item.confirmed > 10000);
 
-  const chart = loadChart(filtered);
+  const chart = generateBarChart(filteredData);
+  const table = generateTable(filteredData);
+  const pieChart = generatePieChart();
 
   document.querySelector('#nextButton').addEventListener('click', () => {
     const currentPage = parseInt(document.querySelector('#currentPage').value);
-    const labels = refreshChartData(chart, filtered, 15, currentPage+1);
+    const labels = refreshChartData(chart, filteredData, 15, currentPage+1);
     if (labels.length > 0) {
       document.querySelector('#currentPage').value = currentPage+1;
     }
@@ -89,10 +151,23 @@ const App = async () => {
     const pageNumberEl = document.querySelector('#currentPage');
     const currentPage = parseInt(pageNumberEl.value);
     if (currentPage > 1) {
-      refreshChartData(chart, filtered, 15, currentPage-1);
+      refreshChartData(chart, filteredData, 15, currentPage-1);
       pageNumberEl.value = currentPage-1;
     }
   });
+
+  document.querySelectorAll('.link').forEach((el) => {
+    el.addEventListener('click', async () => {
+      const country = el.id;
+      const { data } = await fetchData(`http://localhost:3000/api/countries/${country}`);
+      document.querySelector('.modal-title').textContent = data.location;
+      const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+      myModal.show();
+      pieChart.data.datasets[0].data = [data.confirmed, data.deaths];
+      pieChart.update();
+
+    });
+  })
 }
 
 App();
